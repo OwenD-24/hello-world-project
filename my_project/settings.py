8bd0001d79da568699c0903ab0f35251c0b5e3a9
Owen Davis <owendavis1991@gmail.com>
2024-09-06 14:36:49 +0000

    "made a correction to URL"

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%biha04k%l5m!yi-s%fif$nj(-ss!ui7o!l##od#hf9gz51v_*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-owend24-helloworldproje-gqems1gfucw.ws.codeinstitute-ide.net/hello','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-owend24-helloworldproje-gqems1gfucw.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition
